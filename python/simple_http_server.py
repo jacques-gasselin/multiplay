e7137016cfe2de6f0a256fdd34aedcf486815932
Jacques Gasselin <jacques.gasselin@gmail.com>
2020-04-25 02:06:13 -0700

    updated docs and simple HTTP server.

--- python/simple_http_server.py
+++ python/simple_http_server.py
@@ -1,26 +1,81 @@
 import http
 import http.server
+import uuid
+import urllib.parse
+import json
 
-class ServerIntance(object):
-    def connect(self, game):
-        print ("CONNECT game %s" % game)
-        connectionToken = 0
-        return { "connectionToken" : connectionToken }
+class ServerInstance(object):
+    def __init__(self):
+        self.__gameByConnection = {}
+        self.__localPlayerByConnection = {}
+        self.__deviceByConnection = {}
+        self.__playerByLocalPlayerAndConnection = {}
+        self.__playerByDevice = {}
+        self.__dataPerPlayerAndGame = {}
 
-    def login(self, connection, localDevice):
-        print ("LOGIN connection %s on device %s " % (connection, localDevice))
-        localPlayerToken = 0
-        return { "localPlayerToken" : localPlayerToken }
+    def connect(self, handler, game):
+        print("CONNECT game %s" % game)
+        gameUUID = uuid.UUID(game)
+        connectionUUID = uuid.uuid5(gameUUID, handler.client_address[0])
+        self.__gameByConnection[connectionUUID] = gameUUID
+        return { "connectionToken" : str(connectionUUID) }
+
+    def login(self, handler, connection, localDevice):
+        print("LOGIN connection %s on device %s " % (connection, localDevice))
+        #print("__localPlayerByConnection = ", self.__localPlayerByConnection)
+        localDeviceUUID = uuid.UUID(localDevice)
+        connectionUUID = uuid.UUID(connection)
+        self.__deviceByConnection[connectionUUID] = localDeviceUUID
+        if connectionUUID in self.__localPlayerByConnection:
+            print("  found local player for connection")
+            localPlayerUUID = self.__localPlayerByConnection[connectionUUID]
+            return { "localPlayerToken" : str(localPlayerUUID) }
+        if localDeviceUUID in self.__playerByDevice:
+            print("  player exists for device")
+            playerUUID = self.__playerByDevice[localDeviceUUID]
+        else:
+            print("  create new player for device")
+            playerUUID = uuid.uuid4()
+            self.__playerByDevice[localDeviceUUID] = playerUUID
+        localPlayerUUID = uuid.uuid5(localDeviceUUID, str(playerUUID))
+        self.__localPlayerByConnection[connectionUUID] = localPlayerUUID
+        self.__playerByLocalPlayerAndConnection[(localPlayerUUID, connectionUUID)] = playerUUID
+        return { "localPlayerToken" : str(localPlayerUUID) }
+
+    def writePlayerData(self, handler, connection, localPlayer, data):
+        print("WRITE PLAYER DATA '%s' for player %s on connection %s " % (str(data), localPlayer, connection))
+        connectionUUID = uuid.UUID(connection)
+        localPlayerUUID = uuid.UUID(localPlayer)
+        playerUUID = self.__playerByLocalPlayerAndConnection[(localPlayerUUID, connectionUUID)]
+        gameUUID = self.__gameByConnection[connectionUUID]
+        self.__dataPerPlayerAndGame[(playerUUID, gameUUID)] = data
+        return { "status" : 1 }
+
+    def readPlayerData(self, handler, connection, localPlayer):
+        print("READ PLAYER DATA for player %s on connection %s " % (localPlayer, connection))
+        connectionUUID = uuid.UUID(connection)
+        localPlayerUUID = uuid.UUID(localPlayer)
+        playerUUID = self.__playerByLocalPlayerAndConnection[(localPlayerUUID, connectionUUID)]
+        gameUUID = self.__gameByConnection[connectionUUID]
+        data = self.__dataPerPlayerAndGame[(playerUUID, gameUUID)]
+        return { "data" : data }
+
+import pickle
+__serverInstance = None
+def getServerInstance():
+    global __serverInstance
+    if not __serverInstance:
+        try:
+            with open(".simple_http_server_instance", "rb") as f:
+                __serverInstance = pickle.load(f)
+        except FileNotFoundError:
+            pass
+        if not __serverInstance:
+            __serverInstance = ServerInstance()
+    return __serverInstance
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     
-    def getServerInstance(self):
-        try:
-            return self.__serverInstance
-        except AttributeError:
-            self.__serverInstance = ServerIntance()
-            return self.__serverInstance
-
     def _parse_GET(self):
         command, argumentString = self.path.split("?")
         argumentValueByName = {}
@@ -32,16 +87,14 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
                 except ValueError:
                     name = arg
                     value = ""
-                argumentValueByName[name] = value
+                argumentValueByName[name] = urllib.parse.unquote_plus(value)
         return command, argumentValueByName
     
     def _GET_json(self, response):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
-        
-        lst = ['"%s" : "%s"' % (name, value) for name, value in response.items()]
-        self.wfile.write(("{ %s } " % ", ".join(lst)).encode())
+        self.wfile.write(("%s\n" % json.dumps(response)).encode())
 
     def _GET_html(self, response):
         self.send_response(200)
@@ -49,47 +102,55 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
         
         lst = ['%s=%s' % (name, value) for name, value in response.items()]
-        self.wfile.write(("%s" % "\n".join(lst)).encode())
+        self.wfile.write(("%s\n"% "\n".join(lst)).encode())
+
+    def _respond_error(self, code, error, format="json"):
+        self.send_response(code)
+        if format == "json":
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(('{ "error" : "%s" }\n' % str(error)).encode())
+        else:
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(('error=%s\n' % str(error)).encode())
 
     def do_GET(self):
         print("GET")
         try:
             command, argumentValueByName = self._parse_GET()
         except ValueError as e:
-            print(e)
-            self.send_response(400)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
+            self._respond_error(400, e)
             return
-        format = argumentValueByName.pop("response", "html")
-        instance = self.getServerInstance()
+        format = argumentValueByName.pop("response", "json")
+        instance = getServerInstance()
         func = getattr(instance, command[1:])
         try:
-            response = func(**argumentValueByName)
+            response = func(self, **argumentValueByName)
         except TypeError as e:
-            self.send_response(400)
-            if format == "json":
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-                self.wfile.write(('{ "error" : "%s" }' % str(e)).encode())
-            else:
-                self.send_header("Content-type", "text/html")
-                self.end_headers()
-                self.wfile.write(('<html><body>\nerror=%s\n</body></html>' % str(e)).encode())
+            self._respond_error(400, e, format)
             return
-        if format == "json":
-            self._GET_json(response)
-        else:
+        except ValueError as e:
+            self._respond_error(400, e, format)
+            return
+        if format == "html":
             self._GET_html(response)
+        else:
+            self._GET_json(response)
 
     def do_POST(self):
         print("POST")
 
 def run():
     server_address = ('', 12345)
-    with http.server.HTTPServer(server_address, RequestHandler) as httpd:
-        print("serving at ", httpd.server_address)
-        httpd.serve_forever()
+    try:
+        with http.server.HTTPServer(server_address, RequestHandler) as httpd:
+            print("serving at ", httpd.server_address)
+            httpd.serve_forever()
+    except:
+        if __serverInstance:
+            with open(".simple_http_server_instance", "wb") as f:
+                pickle.dump(__serverInstance, f)
 
 if __name__ == "__main__":
     run()
