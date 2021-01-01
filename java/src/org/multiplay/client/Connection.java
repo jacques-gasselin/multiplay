324b841439f6ca247e4c80cdb4ccf5ed4a62f5ab
Jacques Gasselin <jacques.gasselin@gmail.com>
2020-12-31 19:05:49 -0800

    cleaned up fetch logic.

--- java/src/org/multiplay/client/Connection.java
+++ java/src/org/multiplay/client/Connection.java
@@ -6,11 +6,16 @@ import org.multiplay.GameToken;
 
 import com.owlike.genson.Genson;
 import com.owlike.genson.GensonBuilder;
-import org.multiplay.LocalPlayerToken;
+import org.multiplay.client.response.ConnectionResponse;
+import org.multiplay.client.response.LoginResponse;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.NetworkInterface;
 import java.net.URL;
+import java.util.Enumeration;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutorService;
@@ -26,9 +31,34 @@ public abstract class Connection {
 
     private Genson jsonDeserializer = null;
 
+    private static DeviceToken getDefaultDeviceToken() {
+        UUID deviceUUID = UUID.randomUUID();
+        try {
+            Enumeration<NetworkInterface> iter = NetworkInterface.getNetworkInterfaces();
+            while (iter.hasMoreElements()){
+                NetworkInterface ni = iter.nextElement();
+                byte[] macBytes = ni.getHardwareAddress();
+                if (macBytes != null) {
+                    System.out.println("mac address: " + macBytes);
+                    deviceUUID = UUID.nameUUIDFromBytes(macBytes);
+                    break;
+                }
+            }
+        }
+        catch (Throwable t) {
+            t.printStackTrace();
+        }
+        return new DeviceToken(deviceUUID.toString());
+    }
+
     protected Connection(GameToken gameToken, DeviceToken deviceToken, URL serverURL, ExecutorService executor) {
         this.gameToken = gameToken;
-        this.deviceToken = deviceToken;
+        if (deviceToken == null) {
+            this.deviceToken = getDefaultDeviceToken();
+        }
+        else {
+            this.deviceToken = deviceToken;
+        }
         this.serverURL = serverURL;
         if (executor != null) {
             this.executor = executor;
@@ -46,147 +76,24 @@ public abstract class Connection {
         executor.shutdown();
     }
 
-    private static final class ConnectionResponse {
-        private String connectionToken = null;
-
-        public void setConnectionToken(String connectionToken) {
-            this.connectionToken = connectionToken;
-        }
-
-        public String getConnectionToken() {
-            return this.connectionToken;
-        }
-
-        public String toString() {
-            return "connectionToken: " + connectionToken;
-        }
-    }
-
-    /**
-     * Connect to the game's instance of the multiplay service.
-     * @return {Promise} a promise that is fulfilled when the game is connected.
-     */
-    public CompletionStage<Connection> connectAsync() {
-        if (isVerboseLoggingEnabled()) {
-            System.out.println("Connection.connectAsync()");
-        }
-        String protocol = serverURL.getProtocol();
-        String host = serverURL.getHost();
-        int port = serverURL.getPort();
-        return CompletableFuture.supplyAsync(() -> {
-            HttpURLConnection conn = null;
-            try {
-                URL connectUrl = new URL(protocol, host, port, "/connect.json?game=" + gameToken.toString());
-                if (isVerboseLoggingEnabled()) {
-                    System.out.println("Connecting to server at " + connectUrl);
-                }
-                conn = (HttpURLConnection) connectUrl.openConnection();
-                if (isVerboseLoggingEnabled()) {
-                    System.out.println("conn: " + conn);
-                }
-                // grab the response as JSON and get the connection token
-                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                try {
-                    ConnectionResponse response = new ConnectionResponse();
-                    jsonDeserializer.deserializeInto(reader, response);
-                    if (isVerboseLoggingEnabled()) {
-                        System.out.println(response);
-                    }
-                    connectionToken = new ConnectionToken(response.getConnectionToken());
-                }
-                finally {
-                    reader.close();
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace(System.err);
-            }
-            finally {
-                if (conn != null) {
-                    conn.disconnect();
-                }
-            }
-            return this;
-        });
-    }
-
-    private static final class LoginResponse {
-        private String localPlayerToken = null;
-        private String displayName = null;
-        private String friendCode = null;
-        private boolean authenticated = false;
-
-        public String getLocalPlayerToken() {
-            return localPlayerToken;
-        }
-
-        public void setLocalPlayerToken(String localPlayerToken) {
-            this.localPlayerToken = localPlayerToken;
-        }
-
-        public String getDisplayName() {
-            return displayName;
-        }
-
-        public void setDisplayName(String displayName) {
-            this.displayName = displayName;
-        }
-
-        public String getFriendCode() {
-            return friendCode;
-        }
-
-        public void setFriendCode(String friendCode) {
-            this.friendCode = friendCode;
-        }
-
-        public boolean isAuthenticated() {
-            return authenticated;
-        }
-
-        public void setAuthenticated(boolean authenticated) {
-            this.authenticated = authenticated;
-        }
-
-        public String toString() {
-            return "localPlayerToken: " + localPlayerToken + ", displayName: " + displayName + ", friendCode: " + friendCode + ", authenticated: " + authenticated;
-        }
-    }
-
-    public LocalPlayer login() {
-        if (isVerboseLoggingEnabled()) {
-            System.out.println("Connection.login()");
-        }
-
+    <T> T fetchJSONInto(String resource, T response) {
         String protocol = serverURL.getProtocol();
         String host = serverURL.getHost();
         int port = serverURL.getPort();
 
         HttpURLConnection conn = null;
-        LocalPlayer localPlayer = null;
         try {
-            URL loginUrl = new URL(protocol, host, port, "/login.json?connection=" + connectionToken.toString() + "&localDevice=" + deviceToken.toString());
+            URL fetchURL = new URL(protocol, host, port, resource);
             if (isVerboseLoggingEnabled()) {
-                System.out.println("Logging in at " + loginUrl);
+                System.out.println(fetchURL);
             }
-            conn = (HttpURLConnection) loginUrl.openConnection();
-            if (isVerboseLoggingEnabled()) {
-                System.out.println("conn: " + conn);
-            }
+            conn = (HttpURLConnection) fetchURL.openConnection();
             // grab the response as JSON and get the player token and data
-            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            try {
-                LoginResponse response = new LoginResponse();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
                 jsonDeserializer.deserializeInto(reader, response);
                 if (isVerboseLoggingEnabled()) {
                     System.out.println(response);
                 }
-                LocalPlayerToken playerToken = new LocalPlayerToken(response.getLocalPlayerToken());
-
-                localPlayer = new LocalPlayer(this, playerToken, response.getDisplayName(), response.getFriendCode(), response.isAuthenticated());
-            }
-            finally {
-                reader.close();
             }
         }
         catch (Throwable t) {
@@ -197,6 +104,35 @@ public abstract class Connection {
                 conn.disconnect();
             }
         }
+        return response;
+    }
+
+    /**
+     * Connect to the game's instance of the multiplay service.
+     * @return {Promise} a promise that is fulfilled when the game is connected.
+     */
+    public CompletionStage<Connection> connectAsync() {
+        if (isVerboseLoggingEnabled()) {
+            System.out.println("Connection.connectAsync()");
+        }
+        return CompletableFuture.supplyAsync(() -> {
+            String resource = "/connect.json?game=" + gameToken;
+            ConnectionResponse response = new ConnectionResponse();
+            fetchJSONInto(resource, response);
+            connectionToken = new ConnectionToken(response.getConnectionToken());
+            return this;
+        });
+    }
+
+    public LocalPlayer login() {
+        if (isVerboseLoggingEnabled()) {
+            System.out.println("Connection.login()");
+        }
+
+        String resource = "/login.json?connection=" + connectionToken.toString() + "&localDevice=" + deviceToken.toString();
+        LoginResponse response = new LoginResponse();
+        fetchJSONInto(resource, response);
+        LocalPlayer localPlayer = new LocalPlayer(this, response.getLocalPlayerToken(), response.getDisplayName(), response.getFriendCode(), response.isAuthenticated());
         return localPlayer;
     }
 
