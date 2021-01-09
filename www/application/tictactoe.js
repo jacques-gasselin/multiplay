b42ebababc5a73ffea486c49db0adac08f065c89
Jacques Gasselin <jacques.gasselin@gmail.com>
2021-01-09 01:58:01 -0800

    cleaned up TTT some more.

--- www/application/tictactoe.js
+++ www/application/tictactoe.js
@@ -1,13 +1,4 @@
 // global scope
-let gameUUID = "00000000-0000-0000-0000-000000000001";
-let connection = new MPBrowserConnection(gameUUID);
-
-var sessions = [];
-
-var localPlayer = null;
-
-var canvas = null;
-
 class Board {
     constructor() {
         this.points = [
@@ -19,22 +10,103 @@ class Board {
 }
 
 class Game {
-    constructor(session) {
-        this.session = session;
+    constructor(name) {
+        this.name = name;
         this.board = new Board();
         this.playerIsX = true;
     }
 }
 
-var game = new Game(null);
+var currentGame = new Game("Default");
 
-function updatePlayer(player) {
-    localPlayer = player;
-    window.requestAnimationFrame(draw);
+/// ----- Multiplay and UI code
+let gameUUID = "00000000-0000-0000-0000-000000000001";
+let connection = new MPBrowserConnection(gameUUID);
+
+var currentSession = null;
+var localPlayer = null;
+
+function loginPressed() {
+    localPlayer.httpAuthenticate()
+    .then(player => {
+        updatePlayer(player);
+    });
+}
+
+function newGamePressed() {
+    let name = prompt("Game name")
+    connection.getLocalPlayer().createSessionWithName(name)
+    .then(newChannel => {
+        updateGameSessions();
+    });
 }
 
 function updateGameSessions() {
+    localPlayer.fetchSessions()
+    .then(gameSessions => {
+        sessions = gameSessions;
+        if (!currentSession && gameSessions) {
+            currentSession = gameSessions[0];
+            updateGameForSession(currentSession);
+        }
+        let ul = document.getElementById("gamesList");
+        result = '';
+        gameSessions.forEach(s => {
+            result = result
+             + '<li>'
+             + s.getDisplayName()
+             + '&nbsp;'
+             + '<button onclick="selectGameBySessionToken(\'' + s.getSessionToken() +'\')"> -> </button>'
+             + '</li>';
+        });
+        ul.innerHTML = result;
+    });
+}
 
+function selectGameBySessionToken(sessionToken) {
+    localPlayer.getSessions().forEach(s => {
+        if (s.getSessionToken() == sessionToken) {
+            currentSession = s;
+            updateGameForSession(s);
+        }
+    });
+}
+
+function updateGameForSession(session) {
+    session.fetchDataAsJSON().then(data => {
+        var game = new Game(session.getDisplayName());
+        if (data.board) {
+            game.board.points = data.board.points;
+            game.playerIsX = data.playerIsX;
+        }
+        currentGame = game;
+        requestAnimationFrame(draw);
+    });
+}
+
+function updateCurrentSession() {
+    updateGameForSession(currentSession);
+}
+
+window.setInterval(updateCurrentSession, 15000);
+
+/// ----- Game Code
+
+var canvas = null;
+
+function updatePlayer(player) {
+    localPlayer = player;
+
+    let displayName = localPlayer.getDisplayName();
+    let pname = document.getElementById("playerName");
+    if (localPlayer.isAuthenticated()) {
+        pname.innerHTML = 'Signed in as ' + displayName;
+    }
+    else {
+        pname.innerHTML = '<button onclick="loginPressed()">Login</button> Guest user ' + displayName;
+    }
+
+    window.requestAnimationFrame(draw);
 }
 
 function mouseDown(event) {
@@ -48,11 +120,12 @@ function mouseDown(event) {
 
     let index = j * 3 + i;
 
-    let points = game.board.points;
+    let points = currentGame.board.points;
 
     if (points[index] == 0) {
-        points[index] = game.playerIsX ? 1 : 2;
-        game.playerIsX = !game.playerIsX;
+        points[index] = currentGame.playerIsX ? 1 : 2;
+        currentGame.playerIsX = !currentGame.playerIsX;
+        currentSession.sendObjectAsJSONData(currentGame);
         requestAnimationFrame(draw);
     }
 }
@@ -103,9 +176,9 @@ function drawBoard(g, board, x, y, width, height) {
 function draw(timestamp) {
     let title = document.getElementById("gameTitle");
     let playerName = (localPlayer != null) ? localPlayer.getDisplayName() : "Player";
-    title.innerHTML = game.playerIsX ?
-        "<b>" + playerName + "</b> vs Opponent" :
-        "" + playerName + " vs <b>Opponent</b>";
+    title.innerHTML = currentGame.playerIsX ?
+        currentGame.name + ": <b>" + playerName + "</b> vs Opponent" :
+        currentGame.name + ": " + playerName + " vs <b>Opponent</b>";
 
     let g = canvas.getContext("2d");
 
@@ -114,7 +187,7 @@ function draw(timestamp) {
 
     g.clearRect(0, 0, width, height);
 
-    drawBoard(g, game.board, 0, 0, width, height);
+    drawBoard(g, currentGame.board, 0, 0, width, height);
 }
 
 function load() {
